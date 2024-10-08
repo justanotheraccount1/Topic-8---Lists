1bb2e7f7d077cc1bcaaadb7643c8f1683fa95eb4
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-08 09:38:48 -0400

    Add project files.

---
+++ Topic 8 - Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Topic_8___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int rand = generator.Next(6);
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith", "Bailey Hines", "Cypher" }; Console.WriteLine(names[1] + " is 'The One'.");
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.WriteLine("There are " + names.Count + " names in my List."); names[3] = "Agent Smith";
+            Console.WriteLine(names[3] + " is the bad guy.");
+            Console.WriteLine(names[names.Count - names.Count]);
+            Console.WriteLine(names[4] + " isn't in The Matrix...");
+            Console.WriteLine(names[names.Count - 1] + " is the real bad guy");
+            Console.WriteLine(names[rand]);
+            Console.ReadLine(); // Keeps program from closing 
+
+        }
+    }
+}
